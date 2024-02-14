c628260c8ccfd5571b59f83587767d3f890091f4
Asakout <achraf.sakout@capgemini.com>
2024-02-14 22:31:44 +0100

    finish first module next we'll add data transfert

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.models';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes : Recipe[] = [
+    new Recipe("A test recipe","this is a simple test","https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg"),
+    new Recipe("B test recipe","this is a simple test","https://images.immediate.co.uk/production/volatile/sites/30/2020/08/prawn_fried_rice-2481925.jpg?quality=90&resize=440,400"),
+    new Recipe("C test recipe","this is a simple test","https://assets.epicurious.com/photos/631f465a3c260b23af8cd2f3/4:3/w_4618,h_3463,c_limit/HerbButterTurkey_RECIPE_090822_2318.jpg")
+];
 
   constructor(){
 
